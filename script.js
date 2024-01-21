f36f34112187e1234cb25febeade638170159f14
Sohan Pal <126942354+SohanP18@users.noreply.github.com>
2024-01-21 19:25:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//locomotive Scroll
+
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('.main'),
+//     smooth: true
+// });
+
+$(function() {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 1,
+      margin: 0,
+      loop: true,
+    });
+  });
+  
+
+  window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+  
+    loader.classList.add("loader--hidden");
+  
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
+  });
